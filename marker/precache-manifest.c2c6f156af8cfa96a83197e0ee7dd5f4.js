cf9af11dc846dc4cb7a2722e82e3decb3e005441
tge <tgeconf@gmail.com>
2020-03-03 11:18:42 +0800

    add marker

---
+++ marker/precache-manifest.c2c6f156af8cfa96a83197e0ee7dd5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb57371c06efebf4a742b3675d6e3fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d61799f51064cb290e7a",
+    "url": "./static/css/2.120d356f.chunk.css"
+  },
+  {
+    "revision": "6fc70ef3e9077859b92c",
+    "url": "./static/css/main.39de5900.chunk.css"
+  },
+  {
+    "revision": "d61799f51064cb290e7a",
+    "url": "./static/js/2.9feac049.chunk.js"
+  },
+  {
+    "revision": "6fc70ef3e9077859b92c",
+    "url": "./static/js/main.a2831927.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "./static/media/jsoneditor-icons.d961fdfa.svg"
+  }
+]);
